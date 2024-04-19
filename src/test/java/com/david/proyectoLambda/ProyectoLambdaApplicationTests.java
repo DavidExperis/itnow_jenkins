ff20978ccfc5b7beb97cc0e310fc610ac4c55987
DavidExperis <167201237+DavidExperis@users.noreply.github.com>
2024-04-19 08:32:43 +0200

    Add files via upload

---
+++ src/test/java/com/david/proyectoLambda/ProyectoLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.proyectoLambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
